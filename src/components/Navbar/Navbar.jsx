f749523c2b63f32e9e7e316945d12c6658a3490a
emuange <emuange@gmail.com>
2026-07-03 16:10:44 +0200

    Style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link to="/">Manu</Link>
-            <Link to="/about">About</Link>
-            <Link to="/photography">Photography</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/contact">Contact</Link>
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/">MANU</Link>
+            </div>
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/photography">Photography</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/blog">Blog</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+            
         </nav>
     );
 }
