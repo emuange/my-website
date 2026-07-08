ff7a74280fb6cf1730c820ffa43d3e31265a4a23
emuange <emuange@gmail.com>
2026-07-08 15:10:16 +0200

    Fixed styling for writing page and article feed

--- src/components/writing/ArticleFeed.jsx
+++ src/components/writing/ArticleFeed.jsx
@@ -1,18 +1,22 @@
 import articles from "../../data/articles";
 import ContentPreview from "../content/ContentPreview";
 
+import "./ArticleFeed.css";
+
 export default function ArticleFeed() {
   return (
-    <section className="article-feed section">
+    <section className="section">
       <div className="container">
 
         <p className="eyebrow">
             Latest Writing
         </p>
 
+        <div className="article-feed">
         {articles.map((article) => (
           <ContentPreview key={article.id} content={article} />
         ))}
+        </div>
       </div>
     </section>
   );
--- src/pages/Writing.jsx
+++ src/pages/Writing.jsx
@@ -3,13 +3,15 @@ import TopicFilter from "../components/writing/TopicFilter";
 import ArticleFeed from "../components/writing/ArticleFeed";
 import StillCurious from "../components/home/StillCurious";
 
+import "./Writing.css";
+
 export default function Writing() {
   return (
-    <>
+    <div className="writing-page">
       <WritingHeader />
       <TopicFilter />
       <ArticleFeed />
       <StillCurious />
-    </>
+    </div>
   );
 }
